434c49e905a6dcb6718252afa63d02b528359e52
hearts <hearts.zhang@gmail.com>
2013-10-04 22:12:01 +0800

    Signed-off-by: hearts <hearts.zhang@gmail.com>

--- keyframes.hpp
+++ keyframes.hpp
@@ -5,6 +5,12 @@
 #include <vector>
 #include <algorithm>
 
+struct keyframe {
+  uint64_t position = 0;
+  uint64_t time = 0;
+  keyframe(uint64_t pos, uint64_t tm) : position(pos), time(tm) {};
+  keyframe() = default;
+};
 struct keyframes {  // todo: implement rvalue copy ctr
   std::vector<uint64_t> positions;
   std::vector<uint64_t> times;
@@ -12,22 +18,22 @@ struct keyframes {  // todo: implement rvalue copy ctr
   void push_fileposition(uint64_t pos){
     positions.push_back(pos);  // ignore return value
   }
-  void push_time(double milli){
-    auto nano = uint64_t(milli * 10000);
+  void push_time(double sec){
+    auto nano = uint64_t(sec * 10000ull * 1000ull);
     times.push_back(nano); // milli to nano
 //    time_index.insert(nano);
   }
-  uint64_t seek(uint64_t nano){
+  keyframe seek(uint64_t nano){
     assert(positions.size() == times.size());
     return binary_search(nano, 0, int32_t(times.size()) - 1);
   }
-  uint64_t binary_search(uint64_t v, int32_t start, int32_t tail){
-    if (start >= tail){
-      return positions[start];
+  keyframe binary_search(uint64_t v, int32_t start, int32_t tail) {
+    if (start >= tail) {
+      return keyframe{ positions[start], times[start] };
     }
-    auto m = (start + tail) /2;
+    auto m = (start + tail) / 2;
     if (times[m] == v)
-      return positions[m];
+      return keyframe{ positions[m], times[m] };
     if (times[m] < v)
       return binary_search(v, m +1, tail);
     else
