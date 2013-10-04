e02bd7d5a872ae2110fae16ecebaaef70cb2139e
hearts <hearts.zhang@gmail.com>
2013-10-04 22:12:30 +0800

    Signed-off-by: hearts <hearts.zhang@gmail.com>

--- flv_meta.hpp
+++ flv_meta.hpp
@@ -5,7 +5,7 @@
 struct flv_meta {
   flv::audio_codec     audiocodecid;  // only aac and mp3 are supported
   flv::video_codec     videocodecid;  // only avc is supported, avc mapped to media_subtype_h264
-  uint32_t              audiodatarate           = 0; //kbits per second
+  uint32_t              audiodatarate           = 0; //bits per second
   uint32_t              audiodelay              = 0; // seconds
   uint16_t              audiosamplesize         = 0; // 8bits /16bits
   uint64_t              audiosize               = 0; // bytes
--- prop_variant.hpp
+++ prop_variant.hpp
@@ -3,6 +3,7 @@
 struct _prop_variant_t : PROPVARIANT {
   _prop_variant_t();
   ~_prop_variant_t();
+  explicit _prop_variant_t(UINT64 v);
   _prop_variant_t(const _prop_variant_t&);
   _prop_variant_t(PROPVARIANT const*);
   _prop_variant_t&operator=(_prop_variant_t&);
